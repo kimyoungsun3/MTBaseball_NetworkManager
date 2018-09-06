4eb96ac723830568448b11be25466550d27cda92
kimyoungsun <kyssmart@naver.com>
2018-09-06 09:57:36 +0900

    Initial commit

---
+++ Assets/ZTest/LevelTest/LevelTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelTest{
+	public class LevelTest : MonoBehaviour {
+
+		int i = 0;
+		int iMax = 500000;
+		int level = -1;
+		void Update () {
+			
+			for (int j = 0; j < 100; j++, i++) {
+				int l = GetLevel (i);
+				if (level != l) {
+					Debug.Log (i + " > " + l);
+				}
+				level = l;
+			}
+		}
+
+		int GetLevel(int _exp){
+			return (int)(Mathf.Sqrt ((_exp - 1) / 100f + 1521 / 4f) - 39 / 2f) + 1;
+			//ROUNDUP(SQRT(1000000/100+1521/4)-39/2,0)
+
+		}
+	}
+
+}
---
+++ Assets/ZTest/ScrollBar/TimeTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeTest : MonoBehaviour {
+	Slider slider;
+	float time;
+	public float GAME_TIME = 5 * 60f;
+	float barValue;
+	float barSafeValue;
+	public Image fg;
+	Color orginalColor;
+
+	void Start () {
+		slider = GetComponent<Slider> ();
+
+		time 			= Time.time;
+		barValue 		= 1f;
+		barSafeValue = Constant.GAME_SAFE_TIME / Constant.GAME_PLAY_TIME;
+		orginalColor = fg.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float _dt = Time.time - time;
+		Debug.Log (_dt + " > ");
+		_dt = Constant.GAME_PLAY_TIME - _dt % Constant.GAME_PLAY_TIME;
+		barValue = _dt / Constant.GAME_PLAY_TIME;
+		Debug.Log (" > " + _dt + " > " + barValue);
+
+		slider.value = barValue;
+		fg.color = orginalColor;
+		if (barValue < barSafeValue) {
+			fg.color = Color.red;
+		}
+	}
+}
